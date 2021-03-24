bc425d7a71f4332c6cd3cf1e7382a28d705fdd15
Jay Wu <>
2021-03-24 17:21:00 +0800

    [Output Management] Setting up HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -6,6 +7,11 @@ module.exports = {
     print: './src/print.js',
   },
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
