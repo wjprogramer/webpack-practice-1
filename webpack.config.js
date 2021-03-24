84ad4b9aeaf4f80e8e4db7137885bcd9d14fc8cc
Jay Wu <>
2021-03-24 16:35:24 +0800

    loading css

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
